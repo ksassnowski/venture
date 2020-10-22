0a870cc6e521421aea85c61d04c8b9db9bd422a9
Kai Sassnowski <me@kai-sassnowski.com>
2020-10-22 11:04:24 +0200

    Add test to verify that parameters get passed on to constructor

--- tests/AbstractWorkflowTest.php
+++ tests/AbstractWorkflowTest.php
@@ -1,6 +1,7 @@
 <?php declare(strict_types=1);
 
 use Stubs\TestAbstractWorkflow;
+use Stubs\WorkflowWithParameter;
 use Sassnowski\Venture\Facades\Workflow;
 
 uses(TestCase::class);
@@ -11,4 +12,14 @@ it('can be started', function () {
     TestAbstractWorkflow::start();
 
     Workflow::assertStarted(TestAbstractWorkflow::class);
+});
+
+it('passes the parameters to the constructor of the workflow', function () {
+    Workflow::fake();
+
+    WorkflowWithParameter::start('::param::');
+
+    Workflow::assertStarted(WorkflowWithParameter::class, function (WorkflowWithParameter $workflow) {
+        return $workflow->something === '::param::';
+    });
 });
