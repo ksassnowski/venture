36734db9e968b941428722fdf48be0da49ca489d
Kai Sassnowski <ksassnowski@users.noreply.github.com>
2021-11-19 10:46:23 +0100

    Update config

--- config/venture.php
+++ config/venture.php
@@ -1,18 +1,33 @@
 <?php declare(strict_types=1);
 
 use Sassnowski\Venture\ClassNameStepIdGenerator;
+use Sassnowski\Venture\UnserializeJobExtractor;
 
 return [
+    /*
+     * The name of the table Venture uses to store workflows.
+     */
     'workflow_table' => 'workflows',
 
+    /*
+     * The name of the table Venture uses to store workflow jobs.
+     */
     'jobs_table' => 'workflow_jobs',
 
     /*
-     * The class that should be used to generate an id for a
-     * workflow job if no explicit id was provided. In most cases,
-     * you won't have to change this.
+     * The class that should be used to generate an id for a workflow
+     * job if no explicit id was provided. In most cases, you won't have
+     * to change this.
      *
-     * This class needs to implement the `Sassnowski\Venture\StepIdGenerator` interface.
+     * Needs to implement `Sassnowski\Venture\StepIdGenerator`.
      */
     'workflow_step_id_generator_class' => ClassNameStepIdGenerator::class,
+
+    /*
+     * The class that should be used to extract the workflow job instance
+     * from a Laravel queue job. In most cases, you won't have to change this.
+     *
+     * Needs to implement `Sassnowski\Venture\JobExtractor`.
+     */
+    'workflow_job_extractor_class' => UnserializeJobExtractor::class,
 ];
