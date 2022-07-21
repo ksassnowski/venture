360cf3188981b75826f47617d10c246a875fde1f
Kai Sassnowski <me@kai-sassnowski.com>
2022-07-21 06:53:16 +0200

    Remove `opis/closure`

--- tests/BackwardsCompatibilityTest.php
+++ tests/BackwardsCompatibilityTest.php
@@ -11,7 +11,6 @@ declare(strict_types=1);
  * @see https://github.com/ksassnowski/venture
  */
 
-use Opis\Closure\SerializableClosure;
 use Sassnowski\Venture\ClassNameStepIdGenerator;
 use Sassnowski\Venture\JobExtractor;
 use Sassnowski\Venture\Manager\WorkflowManager;
@@ -20,7 +19,6 @@ use Sassnowski\Venture\Serializer\WorkflowJobSerializer;
 use Sassnowski\Venture\StepIdGenerator;
 use Sassnowski\Venture\UnserializeJobExtractor;
 use Stubs\LegacyWorkflowJob;
-use Stubs\TestJob1;
 
 uses(TestCase::class);
 
@@ -56,30 +54,6 @@ it('can handle missing class keys in config', function (string $abstract, string
         Base64WorkflowSerializer::class,
     ],
 ]);
-
-it('can handle old workflows that still use opis/closure for their then_callback', function (): void {
-    $workflow = createWorkflow([
-        'then_callback' => \serialize(SerializableClosure::from(function (): void {
-            ++$_SERVER['__then_called'];
-        })),
-    ]);
-
-    $workflow->runThenCallback();
-
-    expect($_SERVER['__then_called'])->toBe(1);
-});
-
-it('can handle old workflows that still use opis/closure for their catch_callback', function (): void {
-    $workflow = createWorkflow([
-        'catch_callback' => \serialize(SerializableClosure::from(function (): void {
-            ++$_SERVER['__catch_called'];
-        })),
-    ]);
-
-    $workflow->runCatchCallback(new TestJob1(), new Exception());
-
-    expect($_SERVER['__catch_called'])->toBe(1);
-});
 
 it('allows adding jobs to a workflow that do not implement WorkflowStepInterface as long as they use the WorkflowStep trait', function (): void {
     $definition = createDefinition()
