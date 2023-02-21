628043c532aff6785a538c7c1a04ca5beb926e1d
Kai Sassnowski <me@kai-sassnowski.com>
2023-02-21 18:00:41 +0100

    feat: plugin to provide compatibility with lorisleiva/laravel-actions

---
+++ src/Plugin/LaravelActions/UniqueJobDecorator.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2023 Kai Sassnowski
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ *
+ * @see https://github.com/ksassnowski/venture
+ */
+
+namespace Sassnowski\Venture\Plugin\LaravelActions;
+
+use Lorisleiva\Actions\Decorators\UniqueJobDecorator as BaseJobDecorator;
+use Sassnowski\Venture\WorkflowableJob;
+use Sassnowski\Venture\WorkflowStep;
+
+class UniqueJobDecorator extends BaseJobDecorator implements WorkflowableJob
+{
+    use WorkflowStep;
+}
